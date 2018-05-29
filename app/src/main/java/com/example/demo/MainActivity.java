87dbc00757934ed20daa221a317b4f8fd2c2ad3b
avit4799 <avit4799@163.com>
2018-05-29 23:23:58 +0800

    可以根据Fragment构造函数传入的参数1,2,3决定生成的Fragment中的新闻列表 1代表今日 2代表收藏夹（目前只生成固定内容的新闻列表） 3代表生词推送（目前生成的列表与1相同）

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -91,7 +91,7 @@ public class MainActivity extends AppCompatActivity
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
             //return MainActivity.PlaceholderFragment.newInstance(position + 1);
-            return new NewsCategorizedFragment();
+            return NewsCategorizedFragment.newInstance(position + 1);
         }
 
         @Override
